b6870c8e44747c2d0e3f0a3c9bec87d1a46bae2d
Luis Febro <mr.febro@gmail.com>
2019-08-28 11:40:42 -0400

    Updates

---
+++ precache-manifest.42e180aff08d90d1d622cde6e3c08c8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5799d9edb1d07440a8dfbc572c3a0f",
+    "url": "/website_reactjs-robofriends-searching/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/website_reactjs-robofriends-searching/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dc0c91837ec03ecfed5a",
+    "url": "/website_reactjs-robofriends-searching/static/css/main.11d880d5.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/website_reactjs-robofriends-searching/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "dc0c91837ec03ecfed5a",
+    "url": "/website_reactjs-robofriends-searching/static/js/main.059cebaa.chunk.js"
+  },
+  {
+    "revision": "2160eb66e3a2db02f187",
+    "url": "/website_reactjs-robofriends-searching/static/js/runtime~main.6f7e9229.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/website_reactjs-robofriends-searching/static/media/SEGA.12a3db8b.woff"
+  }
+]);
